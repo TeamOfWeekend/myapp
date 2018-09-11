58f0149523fa71150e0f76ea7b518f9dd1e98f4c
lpf李鹏飞 <lipf@BJHuaHuan.com>
2018-09-11 18:18:40 +0800

    修改一处错误

--- code/test.py
+++ code/test.py
@@ -19,32 +19,60 @@ class TLV_TAG(Enum):
     INT = 2
 
 
-# 取所有枚举成员的keys、values
-print('keys   : ', TLV_TAG.__members__.keys())
-print('values : ', TLV_TAG.__members__.values())
-print('items  : ', TLV_TAG.__members__.items())
+# # 取所有枚举成员的keys、values
+# print('keys   : ', TLV_TAG.__members__.keys())
+# print('values : ', TLV_TAG.__members__.values())
+# print('items  : ', TLV_TAG.__members__.items())
+#
+# # 取枚举成员数量
+# print('length : ', len(TLV_TAG))        # 有警告，但可以用
+# print(TLV_TAG.__len__())
+#
+# # 取值方式
+# print(TLV_TAG.INT)
+# print(TLV_TAG(2))
+# print(TLV_TAG['INT'])
+#
+# # 判断一个枚举值是否属于该枚举
+# print(TLV_TAG.STR in TLV_TAG.__members__.values())
+#
+# # 判断一个字符串是否属于该枚举的keys，有时需要将枚举与字符串进行转换
+# print('INT' in TLV_TAG.__members__.keys())
+#
+# # 输出一个枚举值的name、value
+# print(TLV_TAG.INT.name, ' <----> ', TLV_TAG.INT.value)
+#
+# # 遍历所有枚举值
+# for name, value in TLV_TAG.__members__.items():
+#     print(name, ' ==> ', value)
+#
+#
+# print('减肥来得及、绝地反击的、发动机方式'.split('、'))
+# print(len(''.split('、')))
 
-# 取枚举成员数量
-print('length : ', len(TLV_TAG))        # 有警告，但可以用
-print(TLV_TAG.__len__())
 
-# 取值方式
-print(TLV_TAG.INT)
-print(TLV_TAG(2))
-print(TLV_TAG['INT'])
-
-# 判断一个枚举值是否属于该枚举
-print(TLV_TAG.STR in TLV_TAG.__members__.values())
-
-# 判断一个字符串是否属于该枚举的keys，有时需要将枚举与字符串进行转换
-print('INT' in TLV_TAG.__members__.keys())
-
-# 输出一个枚举值的name、value
-print(TLV_TAG.INT.name, ' <----> ', TLV_TAG.INT.value)
-
-# 遍历所有枚举值
-for name, value in TLV_TAG.__members__.items():
-    print(name, ' ==> ', value)
+class Test:
+    def __init__(self):
+        self.a = 1
+        self.b = 2
+        self.c = 3
 
 
-print('减肥来得及、绝地反击的、发动机方式'.split('、'))
+def list_all(test):
+    for name, value in vars(test).items():
+        # test[name] += 1
+        print('item  : ', name, value)
+
+    for key in vars(test).keys():
+        print('key   : ', key)
+
+    for value in vars(test).values():
+        print('value : ', value)
+
+    for x in dir(test):
+        print('x : ', x)
+
+
+
+test = Test()
+list_all(test)
